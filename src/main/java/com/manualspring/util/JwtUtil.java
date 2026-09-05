58f92cfb6e941ef807d64e7d72409538235835ac
Aleksandre Japharidze <alexanderjapharidze@gmail.com>
2026-09-05 09:48:37 +0400

    Security implemented fully now.

--- src/main/java/com/manualspring/util/JwtUtil.java
+++ src/main/java/com/manualspring/util/JwtUtil.java
@@ -10,7 +10,7 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtUtil {
     private static final long EXPIRATION_TIME = 1000 * 60 * 20;
-    private static final String SECRET = "super-secret-string-that-is-long-enough-for-256-bit";
+    private static final String SECRET = "super-secret-string-that-is-long-enough-for-jwt";
 
     public String generateToken(String username) {
         return Jwts.builder()
